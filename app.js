06ec2804db9c5975387448c6f46307318a3ee0ef
ganesh charan paturu.v.n.s <ganesh@test.com>
2025-12-09 15:16:20 +0530

    v1.0.4

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {data} from './data.js';
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -8,15 +9,40 @@ const port = 3000;
 app.get('/', (req, res) => {
     res.render('home');  //home
 });
+app.get('/home', (req, res) => {
+    res.render('home');  //home
+});
 app.get('/addStudent', (req, res) => {
     res.render('add');  //add student
 });
 app.get('/searchStudent', (req, res) => {
     res.render('search');  //search student
+    res.send("ok");
 });
 app.get('/displaystudents', (req, res) => {
-    res.render('display');  //display students
+    const students = data;
+   // res.render('display', { students: data });//display students
+    res.json(students);
 });
+//--add student--//
+app.post('/addStudent', (req, res) => {
+    const { id, name, cgpa } = req.body;
+   
+    data.push({ id, name, cgpa: parseFloat(cgpa) });
+    res.render('add', { message: 'Student added successfully!' });
+});
+//-search student--//
+app.post('/searchStudent', (req, res) => {
+    const { id } = req.body;
+    const student = data.find(student => student.id == id);  
+    if (student) {
+        //res.render('search', { student });
+        res.send({student});  
+    } else {
+        res.render('search', { message: 'Student not found.' });
+    }
+    });
+//--server--//
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
